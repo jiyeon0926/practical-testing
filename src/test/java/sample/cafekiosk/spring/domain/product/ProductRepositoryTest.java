f86a3fada6a43029b66c62b3d637102a58076055
jiyeon0926 <yeoni9094@naver.com>
2025-08-11 02:32:10 +0900

    test: findLatestProductNumberWhenProductIsEmpty 테스트 코드 작성
    - 가장 마지막으로 저장한 상품의 상품번호를 조회할 때, 상품이 없는 경우에는 null을 반환하는지 테스트

--- src/test/java/sample/cafekiosk/spring/domain/product/ProductRepositoryTest.java
+++ src/test/java/sample/cafekiosk/spring/domain/product/ProductRepositoryTest.java
@@ -119,6 +119,16 @@ class ProductRepositoryTest {
         assertThat(latestProductNumber).isEqualTo("003");
     }
 
+    @DisplayName("가장 마지막으로 저장한 상품의 상품번호를 읽어올 때, 상품이 하나도 없는 경우에는 null을 반환한다.")
+    @Test
+    void findLatestProductNumberWhenProductIsEmpty() {
+        // when
+        String latestProductNumber = productRepository.findLatestProductNumber();
+
+        // then
+        assertThat(latestProductNumber).isNull();
+    }
+
     private Product createProduct(String productNumber, ProductType type, ProductSellingStatus sellingStatus, String name, int price) {
         return Product.builder()
                 .productNumber(productNumber)
